60035e52b723643af842366e62bd9a2e1f1a13f0
LemoZun <marrzun@gmail.com>
2024-09-13 17:56:41 +0900

    feat : 9월 2주차 코딩테스트

---
+++ LemoZun/9_2w/Q159994/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Q159994
+{
+    public class Program
+    {
+        /// <summary>
+        /// Q159994
+        /// 카드뭉치
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/159994
+        /// </summary>
+        public class Solution
+        {
+            public string solution(string[] cards1, string[] cards2, string[] goal)
+            {
+                string answer = "";
+                int card1Index = 0;
+                int card2Index = 0;
+
+                foreach (string word in goal)
+                {
+                    if (card1Index < cards1.Length && cards1[card1Index] == word)
+                    {
+                        card1Index++;
+                    }
+                    else if (card2Index < cards2.Length && cards2[card2Index] == word)
+                    {
+                        card2Index++;
+                    }
+                    else
+                    {
+                        return answer = "No";
+                    }
+                }
+
+                return answer = "Yes";
+            }
+        }
+
+
+            //    for (int i = 0; i<goal.Length;i++)
+            //    {
+
+            //        if (cards1[i] == goal[i])
+            //        {
+            //            cards1[i] = null;
+            //            continue;
+            //        }
+            //        else if(cards2[i] == goal[i])
+            //        {
+            //            cards2[i] = null;
+            //            continue;
+            //        }
+            //        else
+            //        {
+            //            return answer = "NO";
+            //        }
+            //    }
+            //    return answer = "Yes";
+            //
+
+    }
+}
