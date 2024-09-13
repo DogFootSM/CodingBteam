c24075ebbb0f4176a2d6667699714c4db1f2a529
tjsp9830 <mcgr1998@gmail.com>
2024-09-13 18:01:08 +0900

    Try 12930
    12930 문제풀이 시도했으나 실패했습니다

---
+++ LSH_12930.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace BProgrammers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = "try hello world";
+
+            Console.WriteLine(Lessons12930(s));            
+
+        }
+
+        static string Lessons12930(string s)
+        {
+            string answer = "";
+            StringBuilder stBD = new StringBuilder();
+
+            // 1. 문장을 어절단위로 분리해서 배열에 넣음
+            string[] words = s.Split(" ");
+
+            // 2. 배열 길이만큼 음절단위로 분리하는 작업을 수행함
+            for (int i = 0; i < words.Length; i++)
+            {
+                // 2-2. 문제발생: 음절단위로 분리에 실패함
+                string[] syllable = words[i].Split("");
+
+                // 3. 음절의 index가 0이나 짝수면 대문자로, 홀수면 소문자로 변환해서 answer에 더해주기
+                for (int j = 0; j < syllable.Length; j++)
+                {
+                    // 3-2. 문제 발생으로 인해 어절의 index를 모두 0으로 인식해서 단어가 전부 대문자로 변환됨
+                    if (j == 0 || j % 2 == 0)
+                    {
+                        answer += syllable[j].ToUpper();
+                        //stBD.Append(syllable[j].ToUpper());
+                    }
+                    else
+                    {
+                        answer += syllable[j].ToLower();
+                        //stBD.Append(syllable[j].ToLower());
+                    }
+
+                }
+
+                // 4. 어절 사이에는 띄어쓰기 추가하기
+                answer += " ";
+
+            }
+
+            // 5. 더해진 조각들을 반환
+            //answer = stBD.ToString();
+            return answer;
+            // 5-2. 문제 발생으로 인해 "TRY HELLO WORLD "이 변환됨
+
+        }
+
+
+    }
+}
