d23ae14e0c0cfc0bdcc84e16ce5fc93a113de140
dhghgh189 <dhghgh189@gmail.com>
2024-09-20 18:15:38 +0900

    [Add] 2주차 소스 업로드
    12930, 159994

---
+++ 2주차/12930.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Programmers_12930
+{
+    public class Solution
+    {
+        public string solution(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // 우선 모든 문자열을 대문자로 만든다.
+            s = s.ToUpper();
+
+            // 문자열을 공백기준으로 나눈다.
+            string[] words = s.Split(' ');
+
+            foreach (string word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    // 짝수번째는 대문자 그대로, 홀수번째는 소문자로 바꿔 저장
+                    // ascii 코드 상 대문자에 32를 더해주면 소문자다
+                    sb.Append(i % 2 == 0 ? word[i] : (char)(word[i] + 32));
+                }
+                // 한 단어가 완성되고 나면 공백 추가
+                sb.Append(' ');
+            }
+            // 마지막 공백은 제거
+            sb.Remove(sb.Length - 1, 1);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 2주차/159994.cs
@@ -0,0 +1,43 @@
+﻿namespace Programmers_159994
+{
+    public class Solution
+    {
+        public string solution(string[] cards1, string[] cards2, string[] goal)
+        {
+            string answer = "";
+
+            // 결과 판정용
+            bool result = true;
+
+            // 각 카드뭉치의 현재 index
+            int index1 = 0;
+            int index2 = 0;
+
+            // 목표 단어를 순회
+            foreach(string word in goal)
+            {
+                // 1번 카드뭉치의 index번째 단어가 goal에 해당하는지 확인 
+                if (index1 < cards1.Length && cards1[index1] == word)
+                {
+                    index1++;
+                }
+                // 2번 카드뭉치의 index번째 단어가 goal에 해당하는지 확인
+                else if (index2 < cards2.Length && cards2[index2] == word)
+                {
+                    index2++;
+                }
+                // 두 카드뭉치를 뒤져도 goal에 해당하지 않으면 더 이상 진행 불가
+                else
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            // 결과에 따라 문자열 저장
+            answer = result ? "Yes" : "No";
+
+            return answer;
+        }
+    }
+}
